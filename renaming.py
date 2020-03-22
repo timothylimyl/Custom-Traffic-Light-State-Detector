ce70993fb2c48275c2d995f8415dc487e7a08c35
Timothy Lim <timothylimyonglee@gmail.com>
2020-03-22 14:10:34 +1100

    Adding files from local repo

---
+++ renaming.py
@@ -0,0 +1,12 @@
+import os
+
+path = 'unseen_images/'
+
+counter = 1
+for f in os.listdir(path):
+    suffix = f.split('.')[-1]
+    if suffix == 'jpg' or suffix == 'png':
+        new = '{}.{}'.format(str(counter), suffix)
+        os.rename(path + f, path + new)
+        counter = int(counter) + 1
+
